d41f35c7716d585f1e0659b1848f35597eecf192
Duane Jeffers <duane@duanejeffers.com>
2015-01-20 16:56:25 -0600

    Frontend Code Changes
    1) Formatted the settings from the CMS. Jadu has different injectors for each widget and workflow.
    2) With the formatted settings, have rearranged dates to display chronologically.
    3) Table setup.
    
    TODO:
    Figure out current semester to display.
    Add options for Two 8-Weeks, MayMester, and the various Summer Semesters.

--- public.php
+++ public.php
@@ -1,10 +1,9 @@
 <?php
-	//var_dump($settings);
-	$semesters = array(0 => 'Spring',
-					   1 => 'May',
-					   2 => 'Summer',
-					   3 => 'Fall',
-					   4 => 'Winter');
+	$semesters = array('Spring',
+					   'May',
+					   'Summer',
+					   'Fall',
+					   'Winter');
 
 	$terms = array(
 		0 => '16 Week',
@@ -27,28 +26,42 @@
 		12 => 'December'
 	);
 
-	$date_count = array();
+	$date_arrange = array();
 
-	$year = date("Y");
+	// The logic behind this is that if the semester is still Winter (which ends around January 5th), then we still need to show the last year.
+	if(date("n") == 1 && date("j") <= 5) {
+		// Display the last year's dates.
+		$year = ((date("Y")) - 1);
+	} else {
+		$year = date("Y");
+	}
 	$years = array();
-	for($i = -1; $i < 3; $i++) {
+	for($i = 0; $i < 3; $i++) {
 		$y = ($year + $i);
 		$years[] = $y;
 
-		$date_count[$y] = array();
-		foreach($semester as $sem) {
-			$date_count[$y][strtolower($sem)] = 0;
+		$date_arrange[$y] = array();
+		foreach($semesters as $sem) {
+			$date_arrange[$y][strtolower($sem)] = array();
 		}
 	}
 
 	// Midnight the day AFTER today.
 	$current_date = mktime(00, 00, 00, date("n"), (date("j") + 1));
-	
-	// formatting the "settings" so we can display the dates properly.
-	$dates = array();
 
 	$date_fmt = '%s %s, %s';
 
+	// We need to get the settings.
+	if(!isset($_POST['preview'])) {
+		$newSettings = array();
+		foreach($settings as $setting) {
+			$newSettings[$setting->name] = $setting->value;
+		}
+
+		$settings = $newSettings;
+	}
+
+	// Formatting the dates first ...
 	foreach($settings as $i => $val) {
 		// We need to grab the event_year first.
 		if(stripos($i, 'event_year-') !== FALSE) {
@@ -58,64 +71,121 @@
 			$start_date = sprintf($date_fmt, $settings['event_start_month-' . $eventId], $settings['event_start_day-' . $eventId], $val);
 			$start_date = strtotime($start_date);
 
-			if(!empty($settings['event_end_month-' . $eventId])
-				&& !empty($settings['event_end_day-' . $eventId])) {
-				$end_date = sprintf($date_fmt, $settings['event_end_month-' . $eventId], $settings['event_end_day-' . $eventId], $val);
-			} elseif(empty($settings['event_end_month-' . $eventId]) 
-					  && !empty($settings['event_end_day-' . $eventId])) { // same month as the start.
-				$end_date = sprintf($date_fmt, $settings['event_start_month-' . $eventId], $settings['event_end_day-' . $eventId], $val);
-			} elseif(empty($settings['event_end_month-' . $eventId]) 
-					  && empty($settings['event_end_day-' . $eventId])) {
-				$end_date = NULL;
-			}
-
-			if(!is_null($end_date)) {
-				$end_date = strtotime($end_date);
-			}
-
-			if(!is_null($end_date)
-				&& $end_date < $current_date) {
-				// Skip this, as the event has already passed ...
-				continue;
-			}
-
-			if($start_date < $current_date
-				&& is_null($end_date)) {
-				// We need to skip this if this is the only date and it is passed.
-				continue;
-			}
-
-			$dates[$start_date] = array(
-				'event_year' => $val,
-				'event_start_month' => $settings['event_start_month-' . $eventId],
-				'event_start_day' => $settings['event_start_day-' . $eventId],
-				'event_end_month' => $settings['event_end_month-' . $eventId],
-				'event_end_day' => $settings['event_end_day-' . $eventId],
-				'event_name' => $settings['event_name-' . $eventId],
-				'event_semester' => $settings['event_semester-' . $eventId],
-				'event_terms' => explode(',', $settings['event_terms-' . $eventId]),
-				'event_highlight' => $settings['event_highlight-' . $eventId]
+			$date_arrange[$val][$settings['event_semester-' . $eventId]][$start_date] = array(
+				'year' => $val,
+				'start_month' => $settings['event_start_month-' . $eventId],
+				'start_day' => $settings['event_start_day-' . $eventId],
+				'end_month' => $settings['event_end_month-' . $eventId],
+				'end_day' => $settings['event_end_day-' . $eventId],
+				'name' => $settings['event_name-' . $eventId],
+				'semester' => $settings['event_semester-' . $eventId],
+				'terms' => explode(',', $settings['event_terms-' . $eventId]),
+				'highlight' => $settings['event_highlight-' . $eventId]
 			);
-			$date_count[$val][$settings['event_semester-' . $eventId]]++;
-
 		} else
 			continue;
 	}
 
+	$header_str = '<tr class="filter_dates %s header"><th colspan="2">%s Semester %s <span class="term">(All Terms)</span></th></colspan>';
+	$event_str = '<tr class="filter_dates %s event"><td><strong>%s</strong></td><td>%s</td></tr>';
+
+	$option_str = '<option value="%s"%s>%s</option>';
+
 	// Now we need to organize the array based on the dates.
-	ksort($dates);
+	$display_events = array();
 
-	// Here we should filter down the date_count array to get valid years to display in the form.
-	var_dump($date_count);
+	// This will let us set what the current semester for the thing to display.
+	$current_semester = null;
 
-	// Next, we display the form and the event dates based on the semester and year.
-	// Finally, for the filter form to work with terms, the dates will display 
+	foreach($date_arrange as $y => $semesters) {
+		foreach($semesters as $s => $events) {
+			// Sort the events from the first start date to the last.
+			ksort($events);
+
+			// Setup the display_events array.
+			$class = ($y . '_' . $s);
+
+			if(count($events) < 1) {
+				continue; // Skip this if there are no events.
+			}
+
+			// Zebra Counter.
+			$zebra = 1;
+
+			$display_events[] = sprintf($header_str, $class, ucfirst($s), $y);
+
+			// We need to have something that will remove the date for an event that occurs on another previous date.
+			$prev_month = null;
+			$prev_day = null;
+			$prev_timestamp = null;
+			foreach($events as $start => $event) {
+				$classes = array($class);
+
+				$event_date = $event['start_month'] . ' ' . $event['start_day'];
+
+				if(strlen($event['end_month']) !== 0) { // This means that the event ends in the next month.
+					$event_date .= ' - ' . $event['end_month'] . ' ' . $event['end_day'];
+				} elseif(strlen($event['end_month']) === 0 && strlen($event['end_day']) < 0) {
+					$event_date .= ' - ' . $event['end_day'];
+				}
+
+				if($event['start_month'] === $prev_month &&
+				   $event['start_day'] === $prev_day &&
+				   strlen($event['end_month']) === 0 &&
+				   strlen($event['end_day']) === 0) {
+
+				   // This should mean that the event occurs on the same date as the previous day and it isn't a series of days ...
+					$event_date = null;
+				}
+
+				// Add the terms as classes for javascript selection.
+				foreach($event['terms'] as $term_id) {
+					$classes[] = 'term-'.$term_id;
+				}
+
+				if($event['highlight'] === '1') {
+					$classes[] = 'highlightRow';
+				}
+
+				// Adding the zebra highlighting on all even rows.
+				if($zebra %  2 === 0) {
+					$classes[] = 'zebra';
+				}
+
+				$display_events[] = sprintf($event_str, implode(' ', $classes), $event_date, $event['name']);
+
+				$zebra++;
+				$prev_month = $event['start_month'];
+				$prev_day = $event['start_day'];
+			}
+		}
+	}
 ?>
 <div id="filterForm">
+	<span class="selection_dropdown">
+		<select id="filterYear">
+			<?php foreach($years as $y) {
+				printf($option_str, $y, ($y === $year ? ' selected' : ''), $y);
+				} ?>
+		</select>
+	</span>
+	<span class="selection_dropdown">
+		<select id="filterSemester">
+			<?php foreach($semesters as $sem) {
 
+				} ?>
+		</select>
+	</span>
 </div>
 <table id="importantDates">
+	<colgroup>
+		<col style="width: 25%;">
+		<col style="width: 75%">
+	</colgroup>
 	<tbody>
-		<tr class="header filter_dates 2015_fall "></tr>
+		<?php print implode("\n", $display_events) ?>
 	</tbody>
 </table>
+<script type="text/javascript">
+
+</script>
